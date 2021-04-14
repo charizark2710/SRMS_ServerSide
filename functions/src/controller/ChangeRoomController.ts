e05562c9e83faf3c8f3e0bd0a50ae8e5ca0eaa86
thanhNT98 <thanhngo100298@gmail.com>
2021-04-14 10:39:40 +0700

    Update booking

--- functions/src/controller/ChangeRoomController.ts
+++ functions/src/controller/ChangeRoomController.ts
@@ -16,13 +16,15 @@ export class ChangeRoomController {
     }
 
     init() {
-        this.router.get(this.path + '', auth, this.getCurrentRoom);
-       
+        this.router.get(this.path + '/getCurrentRoom', auth, this.getCurrentRoom);
+        this.router.post(this.path + '/sendChangeRoomRequest', auth, this.sendChangeRoomRequest);
+        this.router.put(this.path + '/acceptChangeRoomRequest', auth, this.acceptChangeRoomRequest);
+
     }
 
-    getCurrentRoom= async (request: express.Request, response: express.Response) => {
+    getCurrentRoom = async (request: express.Request, response: express.Response) => {
         try {
-            let result:any={};
+            let result: any = {};
             const currentUser = response.locals.employeeId;
 
             const time = new Date();
@@ -39,22 +41,23 @@ export class ChangeRoomController {
             const sec = tempSec.length === 2 ? tempSec : '0' + tempSec;
             const fullDate = year.concat(month, date);
             const tempFullTime = hours.concat(min, sec, '000');
-            const fullTime=parseInt(tempFullTime);
+            const fullTime = parseInt(tempFullTime);
 
 
             (await calendarSchema.get()).forEach(snapshot => {
-                if(snapshot.key == fullDate){
+                if (snapshot.key == fullDate) {
                     snapshot.forEach((calendar) => {
                         //tìm room hiện tại của current user
-                        if(calendar.val().userId == currentUser){
-                            let from=parseInt(calendar.val().from)
-                            let to=parseInt(calendar.val().to)
-                            if(fullTime>=from && fullTime <= to && !calendar.val().isDone){
-                                result={
-                                    id:calendar.key,
-                                    room:calendar.val().room,
-                                    from:calendar.val().from,
-                                    to:calendar.val().to,
+                        if (calendar.val().userId == currentUser) {
+                            let from = parseInt(calendar.val().from)
+                            let to = parseInt(calendar.val().to)
+                            if (fullTime >= from && fullTime <= to && !calendar.val().isDone) {
+                                result = {
+                                    id: calendar.key,
+                                    room: calendar.val().room,
+                                    from: calendar.val().from,
+                                    date: calendar.val().date,
+                                    to: calendar.val().to,
                                 }
                             }
                         }
@@ -75,9 +78,9 @@ export class ChangeRoomController {
 
 
     //1. send request to change room.
-    sendChangeRoomRequest=async (request: express.Request, response: express.Response) => {
+    sendChangeRoomRequest = async (request: express.Request, response: express.Response) => {
         try {
-            const data = request.body; //id trong calendar, userId, room
+            const data = request.body; //id trong calendar, userId, room, date
 
             //tạo ID
             const time = new Date();
@@ -100,7 +103,7 @@ export class ChangeRoomController {
 
             //gửi cho admin
             notification.sendMessage({
-                message: ' sent a request to change room '+data.room,
+                message: ' sent a request to change room ' + data.room,
                 receiver: "admin",
                 sender: data.userId,
                 sendAt: fullTime,
@@ -118,7 +121,7 @@ export class ChangeRoomController {
                 isRead: false,
                 id: id,
             });
-               
+
             return response.status(200).json("ok");
         } catch (err) {
             response.status(500).send(err);
@@ -126,20 +129,20 @@ export class ChangeRoomController {
     }
 
 
-    //2.click vô noti, load data -> calendar controller
+    //2. click vô noti, load data -> calendar controller
 
     //3. load phòng trống => bookroom controller
 
     //4. accept đổi phòng
-      //4.1 update room trong booking
-      //4.2 update isDone trong calendar
-      //4.3 new mới trong calendar
-      //4.4 noti
+    //4.1 update room trong booking
+    //4.2 update isDone trong calendar
+    //4.3 new mới trong calendar
+    //4.4 noti
 
-    acceptChangeRoomRequest= async (request: express.Request, response: express.Response) => {
+    acceptChangeRoomRequest = async (request: express.Request, response: express.Response) => {
         try {
             let currentBookingId;
-            const data = request.body; //id trong calendar, userId, current room, newRoom
+            const data = request.body; //data trong calendar, userId, newRoom
             //tạo ID
             const time = new Date();
             const tempM = (time.getMonth() + 1).toString();
@@ -157,55 +160,94 @@ export class ChangeRoomController {
             tempMs.length === 1 ? tempMs + '0' : tempMs;
             const ms = tempMs.length === 3 ? tempMs : '0' + tempMs;
             const fullTime = year.concat(month, date) + "-" + hours.concat(min, sec, ms);
-            const id = data.userId.toString() + '-' + fullTime;//tránh trùng lịch bị overrride + dễ truy vấn khi xem chi tiết
+            const id = data.userId.toString() + '-' + fullTime;
+
+            let isOcc: boolean = false;
+            const reqFrom = parseInt(data.from);
+            const reqTo = parseInt(data.to);
 
             //4.1 update room trong booking
-            (await db.ref('booking').get()).forEach((snap)=>{
-                if(snap.val().startTime == data.startTime && snap.val().endTime == data.endTime && 
-                    snap.val().date == data.date && snap.val().status == "accepted"){
-                        currentBookingId=snap.key;
+            (await db.ref('booking').get()).forEach((snap) => {
+                if (snap.val().startTime == data.startTime && snap.val().endTime == data.endTime &&
+                    snap.val().date == data.date && snap.val().status == "accepted") {
+                    currentBookingId = snap.key;
+                    return true;
                 }
             })
-            if(currentBookingId){
+            if (currentBookingId) {
                 await db.ref('booking').child(currentBookingId).update({
-                    roomName:data.newRoom,
+                    roomName: data.newRoom,
                 })
             }
 
-            //4.2 update isDone trong calendar
 
 
-
-
-
-
-            //gửi cho admin
-            notification.sendMessage({
-                message: ' sent a request to change room '+data.room,
-                receiver: "admin",
-                sender: data.userId,
-                sendAt: fullTime,
-                isRead: false,
-                id: id,
-                url: "/changeRoomRequest/" + data.id,
-                isValid: true,
+            isOcc = (await calendarSchema.child(data.date).get()).forEach(snap => {
+                const value: Calendar = snap.val();
+                const from = parseInt(value.from);
+                const to = parseInt(value.to);
+                if (!value.isDone) {
+                    if (value.date === data.date && value.room === data.room) {
+                        if (reqFrom === from || reqTo === to) {
+                            return true;
+                        } else if (reqFrom > from && reqFrom < to) {
+                            return true;
+                        } else if (reqTo > from && reqTo < to) {
+                            return true;
+                        } else if (reqFrom < from && reqTo > to) {
+                            return true;
+                        }
+                    }
+                }
             });
-            //gửi cho chính user đặt phòng
-            notification.sendMessage({
-                message: 'Your request to change room ' + data.room + ' is being processed',
-                receiver: data.userId,
-                sender: "admin",
-                sendAt: fullTime,
-                isRead: false,
-                id: id,
-            });
-               
+            if (!isOcc) {
+                (calendarSchema.child(date).child(data.calendarId)).get().then((snapshot) => {
+                    if (snapshot.exists()) {
+                        const val: Calendar = snapshot.val();
+                        const userName = val.userName;
+                        const userId = val.userId;
+                        
+                        //4.2 update isDone trong calendar
+                        calendarSchema.child(date).child(data.calendarId).update({
+                            isDone: true
+                        })
+                        //4.3 new mới trong calendar
+                        calendarSchema.child(data.date).child(data.room.concat('-', data.from, '-', data.to)).set({
+                            date: val.date,
+                            from: val.from,
+                            to: val.to,
+                            isDone: false,
+                            reason: data.reason,
+                            room: data.newRoom,
+                            userId: userId,
+                            userName: userName,//
+                        });
+                        
+                        //4.4 gửi noti cho user
+                        notification.sendMessage({
+                            message: 'You are accepted to change from ' + data.room + ' to '+data.newRoom,
+                            receiver: userId,
+                            sender: "admin",
+                            sendAt: fullTime,
+                            isRead: false,
+                            id: id,
+                        });
+                    } else {
+                        console.log("No data available");
+                    }
+                }).catch((error) => {
+                    console.error(error);
+                });
+            } else {
+                response.status(400).send('Lich kin roi');
+            }
+
             return response.status(200).json("ok");
         } catch (err) {
             response.status(500).send(err);
         }
     }
 
-    
+
 
 }
